392f54534380d94f3140ef67bc06f04f256f076b
Casperdroid5 <caspertak1@gmail.com>
2023-05-11 11:06:51 +0200

    added doxygen commentary to code

--- waste_bin_controller/waste_bin_controller.ino
+++ waste_bin_controller/waste_bin_controller.ino
@@ -1,49 +1,52 @@
 #include "waste_bin_controller.h"
 
 // WiFi settings
-const char* SSID = "WWegvanons3";
-const char* PASSWORD = "JuCasSan27@#";
+const char* SSID = "WWegvanons3";       ///< SSID of the WiFi network
+const char* PASSWORD = "JuCasSan27@#";  ///< Password for the WiFi network
 
 // MQTT Broker settings
-const char* MQTT_SERVER = "192.168.111.237";
-const char* MQTT_USERNAME = "mqtt";
-const char* MQTT_PASSWORD = "WasteBin5#";
-const int MQTT_PORT = 1883;
+const char* MQTT_SERVER = "192.168.111.237";  ///< MQTT broker server IP address
+const char* MQTT_USERNAME = "mqtt";           ///< MQTT broker username
+const char* MQTT_PASSWORD = "WasteBin5#";     ///< MQTT broker password
+const int MQTT_PORT = 1883;                   ///< MQTT broker port number
 
-const char* CLIENT_NAME = "ESP32WasteBin";
+const char* CLIENT_NAME = "ESP32WasteBin";  ///< Name of the MQTT client
 
 // MQTT topics
-const char* SENSOR_DATA_TOPIC = "/data/sensors/";
+const char* SENSOR_DATA_TOPIC = "/data/sensors/";  ///< Topic for sensor data
 
 // Device settings
-const uint8_t peltierandfanpin = 3;
-const long interval = 20000;
-int peltierandfanstate = LOW;
+const uint8_t peltierandfanpin = 3;  ///< Pin for Peltier module and fan
+const long interval = 20000;         ///< Interval for Peltier and fan control
+int peltierandfanstate = LOW;        ///< Current state of the Peltier module and fan
 
 // PubSubClient setup
-WiFiClient wifi_client;
-PubSubClient mqtt_client(wifi_client);
+WiFiClient wifi_client;                 ///< WiFi client for MQTT connection
+PubSubClient mqtt_client(wifi_client);  ///< MQTT client
 
 // Other variables
-unsigned long last_msg_sent = 0;
-const unsigned long msg_interval = 15000;
-unsigned long previousMillis = 0;
+unsigned long last_msg_sent = 0;           ///< Time when the last MQTT message was sent
+const unsigned long msg_interval = 15000;  ///< Interval for sending MQTT messages
+unsigned long previousMillis = 0;          ///< Previous timestamp for interval calculations
 
-const int setPoint = 15;
-const int tolerance = 2;
-const int pwmFreq = 200;
+const int setPoint = 15;  ///< Temperature set point in Celsius
+const int tolerance = 2;  ///< Temperature tolerance in Celsius
+const int pwmFreq = 200;  ///< PWM frequency in Hz
 
 // Sensor pin
-const int BUTTON_PIN = 4;
+const int BUTTON_PIN = 4;  ///< Pin for the lid position sensor
 
 // Sensor values
-bool lid_position = false;
-float humidity_value = 0.0;
-float temperature_value = 0.0;
+bool lid_position = false;      ///< Current state of the lid position
+float humidity_value = 0.0;     ///< Current humidity value
+float temperature_value = 0.0;  ///< Current temperature value
 
 // SHT4x sensor setup
-Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();  ///< SHT4x sensor object
 
+/**
+ * @brief Set up the WiFi connection.
+ */
 void setupWiFi() {
   Serial.print("Connecting to ");
   Serial.println(SSID);
@@ -57,16 +60,22 @@ void setupWiFi() {
     Serial.print("trying to connect... ");
   }
 
-    Serial.println("\nWiFi connected\nIP address: ");
+  Serial.println("\nWiFi connected\nIP address: ");
   Serial.println(WiFi.localIP());
 }
 
+/**
+ * @brief Set up the Peltier module and fan pin.
+ */
 void setupPeltierAndFan() {
   // Peltier + fan pin
   pinMode(peltierandfanpin, OUTPUT);
   analogWriteFrequency(pwmFreq);
 }
 
+/**
+ * @brief Set up the SHT4x sensor.
+ */
 void setupSHT4x() {
   // SHT4x sensor setup
   Serial.println("Adafruit SHT4x test");
@@ -81,12 +90,18 @@ void setupSHT4x() {
   sht4.setHeater(SHT4X_NO_HEATER);
 }
 
+/**
+ * @brief Set up the MQTT client.
+ */
 void setupMQTTClient() {
   // MQTT client setup
   mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
   mqtt_client.setCallback(handleIncomingMessage);
 }
 
+/**
+ * @brief Read sensor data from the SHT4x sensor.
+ */
 void readSensorData() {
   // Read sensor data
   sensors_event_t humidity, temp;
@@ -99,6 +114,9 @@ void readSensorData() {
   temperature_value = temp.temperature;
 }
 
+/**
+ * @brief Update the current lid position based on the button state.
+ */
 void updateLidPosition() {
   // Get lid position
   bool button_state = digitalRead(BUTTON_PIN);
@@ -109,6 +127,9 @@ void updateLidPosition() {
   }
 }
 
+/**
+ * @brief Control the Peltier module based on the temperature error.
+ */
 void controlPeltierModule() {
   float error = setPoint - temperature_value;  // Calculate error
   Serial.println(temperature_value);
@@ -124,6 +145,9 @@ void controlPeltierModule() {
   // Otherwise, maintain current state of Peltier module
 }
 
+/**
+ * @brief Publish sensor data to the MQTT broker.
+ */
 void publishSensorData() {
   // Publish sensor data to MQTT broker
   if (mqtt_client.connected()) {
@@ -142,6 +166,12 @@ void publishSensorData() {
   mqtt_client.loop();
 }
 
+/**
+ * @brief Handle incoming MQTT messages.
+ * @param topic The topic of the incoming message.
+ * @param payload The payload of the incoming message.
+ * @param length The length of the payload.
+ */
 void handleIncomingMessage(char* topic, byte* payload, unsigned int length) {
   // Handle incoming MQTT messages
   String incoming_message = "";
@@ -151,12 +181,19 @@ void handleIncomingMessage(char* topic, byte* payload, unsigned int length) {
   Serial.println("Message arrived [" + String(topic) + "]: " + incoming_message);
 }
 
+/**
+ * @brief Publish a message to the MQTT broker.
+ * @param topic The topic to publish the message to.
+ * @param temperature The temperature value to include in the message.
+ * @param humidity The humidity value to include in the message.
+ * @param lid_position The lid position value to include in the message.
+ */
 void publishMessage(const char* topic, const float temperature, const float humidity, const bool lid_position) {
   // Create JSON object
   auto macAdress = WiFi.macAddress();
   StaticJsonDocument<200> jsonDoc;
 
-    jsonDoc["macAdress"] = macAdress;
+  jsonDoc["macAdress"] = macAdress;
   jsonDoc["temperature"] = temperature;
   jsonDoc["humidity"] = humidity;
   jsonDoc["lid_position"] = lid_position;
@@ -173,6 +210,9 @@ void publishMessage(const char* topic, const float temperature, const float humi
   }
 }
 
+/**
+ * @brief Perform the initial setup of the system.
+ */
 void setup() {
   Serial.begin(115200);
   Serial.print("Connecting to ");
@@ -195,11 +235,12 @@ void setup() {
   setupMQTTClient();
 }
 
+/**
+ * @brief The main program loop.
+ */
 void loop() {
   readSensorData();
   updateLidPosition();
   controlPeltierModule();
   publishSensorData();
 }
-
-
